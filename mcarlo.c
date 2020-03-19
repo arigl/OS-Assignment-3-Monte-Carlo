73b1732d49fbbdb3a64b7b8798fbab87e02eb932
arigl <42983251+arigl@users.noreply.github.com>
2020-03-19 15:25:14 -0700

    Update mcarlo.c

--- mcarlo.c
+++ mcarlo.c
@@ -1,6 +1,21 @@
 /*
-Name: Alex Rigl
-ID: 2300146
+   Name: Alex Rigl
+   ID: 2300146
+   Class: Operating Systems
+   Assignment: #3
+   Email: crigl@chapman.edu
+   Project Description:
+   Write a multithreaded version of this algorithm in C/C++ that creates a 
+   separate thread to generate a number of random points. The thread will 
+   count the number of points that occur within the circle and store that 
+   result in a global variable. When this thread has exited, the parent 
+   thread will calculate and output the estimated value of π. It is worth 
+   experimenting with the number of random points generated. As a general 
+   rule, the greater the number of points, the closer the approximation to π.
+   
+   References:
+   - Random number generation from prompt 
+   - Ishan pthreads calculation
 */
 
 #include <math.h>
